9ddad85e9a0c6a79a6a30bd60f584b7952df20dd
aretecoding <aretecoding@gmail.com>
2021-04-29 22:20:58 -0500

    Created Enemy, enemy movement, and enemy projectile.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    [SerializeField] GameObject pfProjectile;
+
+    [SerializeField] float fireRate;
+    [SerializeField] float fireRateVariation;
+    float firingTimer;
+    
+    [SerializeField] float yMovementThreshhold;
+    [SerializeField] float yRespawnHeight;
+    void Update()
+    {
+        MovementLogic();
+        FiringLogic();
+    }
+
+    private void MovementLogic()
+    {
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+
+        if(transform.position.y < yMovementThreshhold)
+        {
+            float xSpawnPosition = Random.Range(-3f, 3f);
+
+            transform.position = new Vector3(xSpawnPosition, yRespawnHeight, 0);
+        }
+    }
+
+    private void FiringLogic()
+    {
+        firingTimer -= Time.deltaTime;
+
+        if(firingTimer <= 0)
+        {
+            Instantiate(pfProjectile, transform.position, Quaternion.identity);
+            firingTimer = fireRate + Random.Range(-fireRateVariation, fireRateVariation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        if(other.CompareTag("PlayerProjectile"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
